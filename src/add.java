081f6039359266b7992d81d405c1612c272eea57
Raj-Stark <rpal778866@gmail.com>
2022-06-02 11:48:47 +0530

    Made changes in add.java file

--- src/add.java
+++ src/add.java
@@ -1,5 +1,12 @@
 public class add {
     public static void main(String[] args) {
+
+        int a = 10;
+        int b = 19;
+
+        int sum  = a+b;
+
+        System.out.println(sum);
         
     }
 }
